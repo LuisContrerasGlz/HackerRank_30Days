2bfe0afd753fa79f447879974ab1589e446653e9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-28 16:29:56 -0600

    Progress

---
+++ Day4.py
@@ -0,0 +1,34 @@
+"""
+
+Task
+Given an integer, n , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2  to 5, print Not Weird
+If n  is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N % 2 != 0:
+        print("Weird")
+    elif N % 2 == 0 and N in range (2,6):
+        print("Not Weird")
+    elif N % 2 == 0 and N in range (6,21):
+        print("Weird")
+    elif N % 2 == 0 and N > 20:
+        print("Not Weird")
