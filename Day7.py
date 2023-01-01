4e73b6dc87666f4fb8be60987ce92b92883f1a9b
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-01 14:41:24 -0600

    Progress

--- Day7.py
+++ Day7.py
@@ -16,7 +16,12 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-
     arr = list(map(int, input().rstrip().split()))
+    
     for i in reversed(arr):
         print(i, end=" ")
+
+# Option 2
+
+for i in range(len(arr)-1, -1, -1):
+    print(arr[i], end=" ")
