a511b9b23f24b9efe7a8e9cb63a36f00519b4521
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-01 14:27:29 -0600

    Progress

---
+++ Day7.py
@@ -0,0 +1,22 @@
+"""
+
+Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Option 1 with reversed function
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    for i in reversed(arr):
+        print(i, end=" ")
